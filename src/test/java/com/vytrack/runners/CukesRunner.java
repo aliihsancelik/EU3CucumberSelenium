f55a9fad18c8acad5ea82805502a63781fd3a45e
Ali Celik <aliihsancelik1@gmail.com>
2020-10-21 20:56:49 +0100

    new scenarios and feature files added

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         dryRun = false, //dryRun = true won't execute the code inside the definitions, just will check and find the undefined steps!
         // if we put dryRun = false it will be normal execution, so dryRun=true helps us to earn time for finding the undefined steps!
 
-        tags = "@navigate"
+        tags = "@wip"
 
         //tags = "@smoke" //all the tags with smoke gonna execute even on a feature or on a scenario,
                         //you can manage your smoke or regression suite with this way!
@@ -24,6 +24,7 @@ import org.junit.runner.RunWith;
         //tags = "@login and not @sales_manager and not @store_manager" => it means;
                //- which has login and has not sales_manager and has not @store_manager run it (same with exclude)
                //if we put some tag after not: 'not @...' it will run all tags but not the one you wrote after not!
+        //command+shift+F => (this finds the tags and you can delete from whatever you want)
 )
 public class CukesRunner {
 
--- src/test/java/com/vytrack/step_definitions/HOOKS.java
+++ src/test/java/com/vytrack/step_definitions/HOOKS.java
@@ -1,27 +1,36 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
+import java.util.concurrent.TimeUnit;
+
 public class HOOKS {
+
 
     @Before(order = 0) //for all scenarios executes before, order=0 means before everything for @before
     public void setUp(){
-        System.out.println("\tthis is coming from BEFORE");
+
+        Driver.get().manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 
     @After(order = 0) //for all scenarios executes before, order=0 means after everything for @after OPPOSITE of before!!
     public void tearDown(){
-        System.out.println("\tthis is coming from After");
+
+        Driver.closeDriver();
     }
+
 
 
     @Before(value = "@database", order = 1) //custom before: the scenario you put @database before, it runs before
     public void setUpDatabase(){
+
         System.out.println("\tconnecting to database");
     }
     @After(value = "@database", order = 1 ) //custom after: the scenario you put @database before, it runs after
     public void tearDownDatabase(){
+
         System.out.println("\tdisconnecting to database");
     }
 
